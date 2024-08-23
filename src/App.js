188eb8b048c889a9baac9e589c4351744aa78798
ganeshraj0 <ganesh636164@gmail.com>
2024-08-23 01:54:55 -0700

    header

--- src/App.js
+++ src/App.js
@@ -44,17 +44,7 @@ function App() {
   };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         ganesh trianding
-        </p>
-        <a
-
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
     </div>
   );
 }

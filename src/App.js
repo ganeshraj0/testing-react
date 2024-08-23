481141c7c521762dc089bf5fa38ec9b35ac12a15
ganeshraj0 <ganesh636164@gmail.com>
2024-08-23 01:45:53 -0700

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,63 +1,23 @@
-import Header from "./Header";
-import "./App.css";
-import Contant from "./Contant";
-import { useState } from "react";
-import Footer from "./Footer";
-import AddItem from "./AddItem";
-import SearchItem from "./SearchItem";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [item, setItem] = useState(
-    JSON.parse(localStorage.getItem("todo_list"))
-  );
-
-  const [newItem, setNewItem] = useState("");
-  const [search, setSearch] = useState("");
-
-  const addItem = (items) => {
-    const id = item.length ? item[item.length - 1].id + 1 : 1;
-    const addNewItem = { id, checked: false, items };
-    const listItem = [...item, addNewItem];
-    setItem(listItem);
-    localStorage.setItem("todo_list", JSON.stringify(listItem));
-  };
-
-  const handeling = (id) => {
-    const listing = item.map((i) =>
-      i.id === id ? { ...i, checked: !i.checked } : i
-    );
-    setItem(listing);
-    localStorage.setItem("todo_list", JSON.stringify(listing));
-  };
-  const deletefun = (id) => {
-    const deleteitem = item.filter((i) => i.id !== id);
-    setItem(deleteitem);
-    localStorage.setItem("todo_list", JSON.stringify(deleteitem));
-  };
-
-  const sumitItem = (e) => {
-    e.preventDefault();
-    if (!newItem) return;
-    addItem(newItem);
-    setNewItem("");
-  };
   return (
     <div className="App">
-      <Header />
-      <AddItem
-        newItem={newItem}
-        setNewItem={setNewItem}
-        sumitItem={sumitItem}
-      />
-      <SearchItem search={search} setSearch={setSearch} />
-      <Contant
-        item={item.filter((items) =>
-          items.items.toLowerCase().includes(search.toLowerCase())
-        )}
-        handeling={handeling}
-        deletefun={deletefun}
-      />
-      <Footer length={item.length} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         ganesh trianding
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

793bc185acf2d01e1105940653e1af7aa05e4949
ganeshraj0 <ganesh636164@gmail.com>
2024-08-23 03:30:48 -0700

    items

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from "./Header";
-import Contant from "./Contant";
-import { useState } from "react";
-import Footer from "./Footer";
-import AddItem from "./AddItem";
-import SearchItem from "./SearchItem";
+import "./App.css";
+import Items from "./Items";
 
 function App() {
-  const [item, setItem] = useState(
-    JSON.parse(localStorage.getItem("todo_list"))
-  );
-
-  const [newItem, setNewItem] = useState("");
-  const [search, setSearch] = useState("");
-
-  const addItem = (items) => {
-    const id = item.length ? item[item.length - 1].id + 1 : 1;
-    const addNewItem = { id, checked: false, items };
-    const listItem = [...item, addNewItem];
-    setItem(listItem);
-    localStorage.setItem("todo_list", JSON.stringify(listItem));
-  };
-
-  const handeling = (id) => {
-    const listing = item.map((i) =>
-      i.id === id ? { ...i, checked: !i.checked } : i
-    );
-    setItem(listing);
-    localStorage.setItem("todo_list", JSON.stringify(listing));
-  };
-  const deletefun = (id) => {
-    const deleteitem = item.filter((i) => i.id !== id);
-    setItem(deleteitem);
-    localStorage.setItem("todo_list", JSON.stringify(deleteitem));
-  };
-
-  const sumitItem = (e) => {
-    e.preventDefault();
-    if (!newItem) return;
-    addItem(newItem);
-    setNewItem("");
-  };
   return (
-    <div className="App">
-    <Header />
-    <AddItem
-        newItem={newItem}
-        setNewItem={setNewItem}
-        sumitItem={sumitItem}
-      />
+    <div>
+      <Items />
     </div>
   );
 }
---
+++ src/Items.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Header from "./Header";
+import "./App.css";
+import Contant from "./Contant";
+import { useState } from "react";
+import Footer from "./Footer";
+import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
+
+const Items = () => {
+  const [item, setItem] = useState(
+    JSON.parse(localStorage.getItem("todo_list"))
+  );
+
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+
+  const addItem = (items) => {
+    const id = item.length ? item[item.length - 1].id + 1 : 1;
+    const addNewItem = { id, checked: false, items };
+    const listItem = [...item, addNewItem];
+    setItem(listItem);
+    localStorage.setItem("todo_list", JSON.stringify(listItem));
+  };
+
+  const handeling = (id) => {
+    const listing = item.map((i) =>
+      i.id === id ? { ...i, checked: !i.checked } : i
+    );
+    setItem(listing);
+    localStorage.setItem("todo_list", JSON.stringify(listing));
+  };
+  const deletefun = (id) => {
+    const deleteitem = item.filter((i) => i.id !== id);
+    setItem(deleteitem);
+    localStorage.setItem("todo_list", JSON.stringify(deleteitem));
+  };
+
+  const sumitItem = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
+  };
+  return (
+    <div>
+      <div className="App">
+        <Header />
+        <AddItem
+          newItem={newItem}
+          setNewItem={setNewItem}
+          sumitItem={sumitItem}
+        />
+        <SearchItem search={search} setSearch={setSearch} />
+        <Contant
+          item={item.filter((items) =>
+            items.items.toLowerCase().includes(search.toLowerCase())
+          )}
+          handeling={handeling}
+          deletefun={deletefun}
+        />
+        <Footer length={item.length} />
+      </div>
+    </div>
+  );
+};
+
+export default Items;
